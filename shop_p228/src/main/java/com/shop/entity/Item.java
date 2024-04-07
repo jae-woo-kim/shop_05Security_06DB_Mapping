4674267793e92cece558433a283ef4c3f79d8c18
lmcomputer <lmcomputer@naver.com>
2024-04-07 22:32:30 +0900

    시큐리티와 디비연관 관계

---
+++ shop_p228/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,37 @@
+package com.shop.entity;
+
+import com.shop.common.entity.BaseEntity;
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="item_id")
+    private Long id;          //상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;  // 상품명
+
+    @Column(nullable = false)
+    private int price;      // 가격
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
+}
